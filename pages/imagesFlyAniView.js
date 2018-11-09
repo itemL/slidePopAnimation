3ac19295ec5ddd8be07cc75e7959a40456b853ad
huiyanliu <1076325078@qq.com>
2018-11-09 19:44:29 +0800

    first commit

---
+++ pages/imagesFlyAniView.js
@@ -0,0 +1,110 @@
+export var movedirectionStyle = {
+  top:1,
+  left:2,
+  bottom:3,
+  right:4,
+}
+
+class imagesFlyAniView{
+  constructor(pageContext){
+    this.page = pageContext;
+    this.page.data.imageList = [];
+    this.page.data.animations = [];
+    this.isAnimation = false;
+    this.page.__startMoveAnimation = this.__startMoveAnimation.bind(this);
+    this.page.animationend = this.__animationend.bind(this);
+  }
+/*
+
+*/ 
+  load(images, imageSize, moveLength, time, direction){
+   if (!images || images.length <= 0){
+     return;
+   }
+   var imageList = [];
+   for(var i = 0;i<images.length; ++i){
+     var url = images[i];
+     imageList.push({ url: url,ani:""});
+   }
+   
+   this.page.setData({
+     'imageList': imageList
+   })
+   
+   this.imageSize = imageSize && imageSize.width > 0 && imageSize.height > 0 ? imageSize : {width:100,height:100};
+   this.moveLength = moveLength;
+   this.time = 0.5;
+   //direction ? "" : 
+    this.direction = movedirectionStyle.top;
+ }
+
+  __startMoveAnimation(res){
+    console.warn(res);
+    var _this = this;
+    var list = [];
+
+    if (!this.isAnimation){
+      this.isAnimation = true;
+      for (var i = 0; i < this.page.data.imageList.length; ++i) {
+        var item = this.page.data.imageList[i];
+        var moveLength = 50 + 50 * i;
+        item.ani = this.__getAnimation(moveLength);
+        list.push(item)
+      }
+    }else{
+      this.isAnimation = false;
+      for (var i = 0; i < this.page.data.imageList.length; ++i) {
+        var item = this.page.data.imageList[i];
+        var moveLength = 50 + 50 * i;
+        item.ani = this.__getAnimation(0);
+        list.push(item)
+      }
+    }
+
+    this.page.setData({
+      'imageList': list
+    })
+
+    for (var i = 0; i < this.page.data.imageList.length; ++i) {
+      var item = this.page.data.imageList[i];
+      item.ani.export();
+    }
+  
+  }
+
+  __animationend(event){
+    console.warn(event);
+  }
+
+  __getAnimation(length){
+    if (this.direction == movedirectionStyle.top) { 
+      return this.__getAnimationTranslateY(-length);
+    } else if (this.direction == movedirectionStyle.left){
+      return this.__getAnimationTranslateX(length);
+    } else if (this.direction == movedirectionStyle.bottom){
+      return this.__getAnimationTranslateY(-length);
+    }else{
+      return this.__getAnimationTranslateX(length);
+    }
+  }
+
+  __getAnimationTranslateX(translateX){
+    var animation = wx.createAnimation({
+      duration: this.time * 1000,
+      timingFunction: "linear"
+    })
+    animation.translateX(translateX).step();
+    return animation;
+  }
+
+  __getAnimationTranslateY(translateY) {
+    var animation = wx.createAnimation({
+      duration: this.time * 1000,
+      timingFunction: "linear"
+    })
+    animation.translateY(translateY).step();
+    return animation;
+  }
+
+}
+module.exports = imagesFlyAniView;
---
+++ pages/index/index.js
@@ -0,0 +1,65 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var ImagesFlyAniView = require("../imagesFlyAniView.js");
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    
+    var _this = this;
+    _this.imagesFlyAniView = new ImagesFlyAniView(_this);
+    var imageList = ["/images/image.png", "/images/image.png", "/images/image.png"];
+
+    _this.imagesFlyAniView.load(imageList, { width: 100, height: 100 }, 200, 1);
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
+    // this.setData({
+    //   imageList: ["/images/image.png", "/images/image.png", "/images/image.png"]
+    // })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
