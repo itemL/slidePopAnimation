d94897934ad015e5c93a0b30332a11e52915dc0a
huiyanliu <1076325078@qq.com>
2018-11-10 15:18:33 +0800

    "代码整理与优化"

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,21 +4,7 @@ const app = getApp()
 var ImagesFlyAniView = require("../imagesFlyAniView.js");
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-
-clickPopSetsButton
-  : function(event){
-    console.warn("event",event);
+    
   },
 
   onLoad: function () {
@@ -31,40 +17,5 @@ clickPopSetsButton
     _this.imagesFlyAniView.load("/images/image.png", imageList, { width: 80, height: 80 }, 10, 0.5, 3,function(index){
       console.warn(index);
     });
-    
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
